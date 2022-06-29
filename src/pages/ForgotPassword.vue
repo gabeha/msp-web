f0f7adb36767e6ba7b2f4be804db9fe7a18a3298
gabeha <81102597+gabeha@users.noreply.github.com>
2022-06-29 16:43:16 +0200

    reset password

--- src/pages/ForgotPassword.vue
+++ src/pages/ForgotPassword.vue
@@ -10,13 +10,13 @@ const form = ref({
 
 
 const handlePasswordReset = async () => {
-  await sendPasswordRestEmail(form.value);
+  await sendPasswordRestEmail(form.value.email);
   alert("Password reset email sent to: ${form.value}");
 };
 </script>
 <template>
 <div class="w-1/2 mx-auto border-2">
-    <form class="m-auto my-5 pt-10 w-full  text-2xl" @submit.prevent="handlePasswordReset()" >
+    <form class="m-auto my-5 pt-10 w-full  text-2xl" @submit.prevent="handlePasswordReset" >
     <h1 class="text-5xl p-4 text-center w-full mb-10">Forgot Password?</h1>
     <label class=" ml-6 my-12">Email
         <input class="block mx-auto  h-12 bg-gray-200 border-none w-11/12 text-xl rounded-lg my-8" placeholder="E-mail address" v-model="form.email" type="email"/> </label>
--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -58,6 +58,7 @@ const handleSubmit = async () => {
             <input type="password" id="password" v-model="form.password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-sky-500 focus:border-sky-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-sky-500 dark:focus:border-sky-500" placeholder="•••••••••" required="">
         </div>
         <button type="submit" class="text-white bg-sky-700 hover:bg-sky-800 focus:ring-4 focus:outline-none focus:ring-sky-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-sky-600 dark:hover:bg-sky-700 dark:focus:ring-sky-800">Submit</button>
+        
     </form>
         
     <form v-else @submit.prevent="handleSubmit()" class="w-full">
